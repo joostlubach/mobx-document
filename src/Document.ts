52ca2dc6b721665706e90325d2c51bcffd9d2eca
Joost Lubach <joostlubach@gmail.com>
2026-02-03 12:06:25 +0100

    Shallow doc data.

--- src/Document.ts
+++ src/Document.ts
@@ -36,10 +36,10 @@ export default abstract class Document<
 
   // #region Data
 
-  @observable
+  @observable.shallow
   public accessor data: T | null = null
 
-  @observable
+  @observable.shallow
   public accessor meta: M | null = null
 
   protected defaultParams: P
